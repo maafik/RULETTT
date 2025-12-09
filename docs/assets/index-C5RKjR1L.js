bfa1eea9531b27680b2a9c027f74178b73e5a2e7
maafik <maafik@66.ru>
2025-12-09 21:13:32 +0300

    Update web build

---
+++ docs/assets/index-C5RKjR1L.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bse70McD.js","assets/index-CEBXCajX.js","assets/index-BBtmw1lT.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CEBXCajX.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Bse70McD.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
