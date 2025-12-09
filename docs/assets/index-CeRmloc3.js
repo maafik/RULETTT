49ea86ad26e44aef3878fa0457898ee4d37c5124
maafik <maafik@66.ru>
2025-12-09 14:49:00 +0300

    Remove toasts from payment return page

--- docs/assets/index-CeRmloc3.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-PcQqzRAz.js","assets/index-CWgSQMkk.js","assets/index-3FP8uR9x.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CWgSQMkk.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-PcQqzRAz.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
