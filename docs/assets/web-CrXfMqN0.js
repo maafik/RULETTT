bf120555a55ac941a13801c3aacb40805b89cad8
maafik <maafik@66.ru>
2025-12-09 21:42:26 +0300

    Update web build

---
+++ docs/assets/web-CrXfMqN0.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BTxljX5u.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- docs/assets/web-Dah6Z2JS.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-DAXGR9SH.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
