2fc2d2df32a9217f9c629565860e75d3e51e4c60
maafik <maafik@66.ru>
2025-12-09 20:02:16 +0300

    Fix header layout and dialog scroll

---
+++ docs/assets/index-D27aSBUp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BPAW6_50.js","assets/index-LF1-NyVG.js","assets/index-DV8Yww80.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-LF1-NyVG.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-BPAW6_50.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
