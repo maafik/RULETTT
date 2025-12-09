170cc1ace1b7ac3a84682643e0a3ddaa4a40f58f
maafik <maafik@66.ru>
2025-12-09 20:26:55 +0300

    Fix safe-area after payment return

--- docs/assets/index-DpV0qgIL.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CIYSci6O.js","assets/index-BWUoqwiu.js","assets/index-3FP8uR9x.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-BWUoqwiu.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-CIYSci6O.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/web-CCygrmwr.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-CY7yNbDX.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
