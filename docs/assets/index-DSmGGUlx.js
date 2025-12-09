770e66ea4f077059c003d1b6e44b0e4fe28ede16
maafik <maafik@66.ru>
2025-12-09 12:31:00 +0300

    Update payment return behavior

--- docs/assets/index-DSmGGUlx.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cws5b2V-.js","assets/index-Brko4oGf.js","assets/index-3FP8uR9x.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-Brko4oGf.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Cws5b2V-.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
