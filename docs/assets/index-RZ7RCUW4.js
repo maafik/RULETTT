275104c9d3e085fffa923c8604aee0c3df66cb42
maafik <maafik@66.ru>
2025-12-09 13:12:39 +0300

    Update post-payment redirect to orders list

---
+++ docs/assets/index-RZ7RCUW4.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DbBs0_hY.js","assets/index-CDOlmCyg.js","assets/index-3FP8uR9x.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CDOlmCyg.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-DbBs0_hY.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
