34e133f6139914f53cb7a2928257f7511b45a759
maafik <maafik@66.ru>
2025-12-09 13:17:16 +0300

    Add orders deeplink

---
+++ docs/assets/index-CKyQoFCI.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DLB_2UG7.js","assets/index-DjGWoyj4.js","assets/index-3FP8uR9x.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-DjGWoyj4.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-DLB_2UG7.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
