b52c55b249dc3f5ca45d1a1edf7bebc976a45cae
maafik <maafik@66.ru>
2025-12-09 21:20:36 +0300

    Update web build

--- docs/assets/web-Bse70McD.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-CEBXCajX.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ docs/assets/web-kbeVSnpj.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BCLXF-Ny.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
