03532755a8287e849f6b4e692d761eb6ec2a8b21
maafik <maafik@66.ru>
2025-12-09 21:04:23 +0300

    Update web build

---
+++ docs/assets/index-BzEwghLp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dbox4-Co.js","assets/index-CL9VOK8v.js","assets/index-BsxTHIyo.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CL9VOK8v.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Dbox4-Co.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/web-Dbox4-Co.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-CL9VOK8v.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
