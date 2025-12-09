4af8a49cdd8afe34e9b74f24bc06996701982671
maafik <maafik@66.ru>
2025-12-09 20:43:24 +0300

    Update web build

--- docs/assets/index-CYdyJSYp.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CCygrmwr.js","assets/index-CY7yNbDX.js","assets/index-BK8-xmVE.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CY7yNbDX.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-CCygrmwr.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
