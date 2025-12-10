76dfabe6088a505874ff4d0e98330c5591d1ac97
maafik <maafik@66.ru>
2025-12-10 08:33:51 +0300

    Update web build

--- docs/assets/index-BlnNCbCG.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-D3yPm42q.js","assets/index-CVaAYuIH.js","assets/index-AYt2Uhrc.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CVaAYuIH.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-D3yPm42q.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/index-_E6-nj95.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B_wpshuC.js","assets/index-okN5xuLY.js","assets/index-AYt2Uhrc.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-okN5xuLY.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-B_wpshuC.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
