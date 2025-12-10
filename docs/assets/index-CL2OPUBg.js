3e1c76c96217f2242d883674b78fde38d5f64cff
maafik <maafik@66.ru>
2025-12-10 18:15:44 +0300

    Update web build

---
+++ docs/assets/index-CL2OPUBg.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BnTTsPWL.js","assets/index-C1csI_rl.js","assets/index-D0b5KO7m.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-C1csI_rl.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-BnTTsPWL.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/web-BnTTsPWL.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-C1csI_rl.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
