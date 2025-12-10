aa7b846fb5cecb415390bd80d8e41106e0b9f180
maafik <maafik@66.ru>
2025-12-10 18:32:52 +0300

    Update web build

---
+++ docs/assets/index-LlSZFYBl.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cuaqw0fS.js","assets/index-CUPlw8r_.js","assets/index-Ibn96TYG.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-CUPlw8r_.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Cuaqw0fS.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/web-Cuaqw0fS.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-CUPlw8r_.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
