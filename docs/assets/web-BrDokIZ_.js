64e0624333004a64288e871b15b1698d806f8f6c
maafik <maafik@66.ru>
2025-12-10 10:31:38 +0300

    Update web build

--- docs/assets/web-BrDokIZ_.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-Dr4HoZRH.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ docs/assets/web-TaplEpBh.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BE47SStE.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
