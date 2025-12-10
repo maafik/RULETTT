3529e48eeac60c7a547f221e66f91e5acefa255b
maafik <maafik@66.ru>
2025-12-10 08:10:40 +0300

    Update web build

--- docs/assets/index-CAdehADw.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CrXfMqN0.js","assets/index-BTxljX5u.js","assets/index-AYt2Uhrc.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-BTxljX5u.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-CrXfMqN0.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
