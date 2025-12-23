741f00e403ec5b5cb3b58d0d6b88a69105367d65
maafik <maafik@66.ru>
2025-12-23 22:33:24 +0300

    Update web build

--- docs/assets/index-Dc0LMiFs.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BMqyGkEu.js","assets/index-Dp0-txZc.js","assets/index-BhGJmQNB.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-Dp0-txZc.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-BMqyGkEu.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/index-TtKrNeVs.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BNOsH8el.js","assets/index-BUCHCYai.js","assets/index-Bt3ksM6d.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-BUCHCYai.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-BNOsH8el.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
