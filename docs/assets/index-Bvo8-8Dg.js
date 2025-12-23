9d9c0a924c5cc5b390fb6961840ee78a4d934d4e
maafik <maafik@66.ru>
2025-12-23 20:48:25 +0300

    12312

---
+++ docs/assets/index-Bvo8-8Dg.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BfCrjHpw.js","assets/index-O-k8QdpH.js","assets/index-qKPFw57X.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-O-k8QdpH.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-BfCrjHpw.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
