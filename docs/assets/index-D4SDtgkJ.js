487b8701191323e835ef162d612c2ff356d0dd10
maafik <maafik@66.ru>
2025-12-23 23:23:26 +0300

    Update web build

---
+++ docs/assets/index-D4SDtgkJ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dm3VOIc0.js","assets/index-9L3kVtxv.js","assets/index-Bl4t71p2.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-9L3kVtxv.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Dm3VOIc0.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
