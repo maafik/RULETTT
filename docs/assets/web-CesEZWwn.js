94aef79a74bc13ef7c3383513de98335cb42fef2
maafik <maafik@66.ru>
2025-12-26 01:22:28 +0300

    Update web build

---
+++ docs/assets/web-CesEZWwn.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BJyCHj30.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- docs/assets/web-Dm3VOIc0.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-9L3kVtxv.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
