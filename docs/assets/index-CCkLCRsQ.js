b340e8e5e08203d97304baa1ee1df636c31c4f53
maafik <maafik@66.ru>
2025-12-30 20:34:27 +0300

    Update web build

---
+++ docs/assets/index-CCkLCRsQ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dya-tH8c.js","assets/index-6pz9rgaP.js","assets/index-Q8pUMlBC.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-6pz9rgaP.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-Dya-tH8c.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ docs/assets/web-Dya-tH8c.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-6pz9rgaP.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
